6d9f2e5bd8ce0f6137a7f961c5f364a0304d90e5
Ramsha909 <ramsha.bcss19@iba-suk.edu.pk>
2022-10-26 16:18:59 +0500

    Persist searched Data

--- Desktop/mid1labexam/labtask/src/App.js
+++ Desktop/mid1labexam/labtask/src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import createHistory from "history/createBrowserHistory";
 
+const history = createHistory();
 
-
+class Page extends React.Component {
+  state = {
+    value: "",
+    query: "",
+    data: ["React", "funny", "Geo"],
+    filteredData: []
+  };
+  //componentDidMount
+  componenetDidMount(){
+    this.setQuery();
+  }
+  //componentDidUpdate
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.query !== this.props.match.params.query) 
+    {
+      this.setQuery();
+    }
+  }
+  setQuery = () => {
+    const { query = "" } = this.props.match.params;
+    const filteredData = this.state.data.filter(element =>
+      element.toLowerCase().includes(query.toLowerCase())
+    );
+    this.setState({ value: query, query, filteredData });
+  };
+  
 function App() {
   const [data, setdata] = useState({})
 
@@ -12,8 +39,28 @@ function App() {
     setdata( state => ({...jsonData}) 
     
     );
-
   }
+  const {searchQuery,searchResults} = JSON.parse(window.sessionStorage.getItem("searchDetails"));
+  
+  const [d, setd] = useState(searchQuery ? searchQuery : "" );
+   const [result, setResult] = useState( searchResults ? searchResults:[]);
+   const apiKey = ("API")
+   
+   function handleChange(event) {
+       const d = event.target.value;
+       console.log(event.target.value);
+       setd(d);
+   }
+   function handleSubmit(event) {
+       event.preventDefault();
+       axios.get("https://hn.algolia.com/api/v1/search?query=hello&page=0" + d + "&key=" + apiKey + "&maxResults=20")
+           .then(data => {
+               console.log(data.data.items);
+               setResult(data.data.items);
+               window.sessionStorage.setItem("searchDetails",JSON.stringify({searchQuery:d,searchDetails:data.data.items}))
+              })
+      }
+//user click on privious searched items 
 
 useEffect(() => {
     getAPI().then(() => console.log(data));
@@ -21,7 +68,7 @@ useEffect(() => {
   return (
     <div className="App">
       <h1>My Hacker Stories</h1>
-        <label>Search: <input type="text" name ="name" /> <button> submit </button></label>
+        <label>Search: <input type="text" name ="name" /> <button type='submit'> submit </button></label>
         <br></br>
         
         {/* <button type ="submit">React</button>
